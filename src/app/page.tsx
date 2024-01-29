b535a21cc7206266d411e9a07cb147d9ca643ee0
Kelechi Okeke <48678691+invisiblemask@users.noreply.github.com>
2024-01-29 14:38:02 +0100

    chore: edit done on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <section>Home</section>;
+  return (
+    <section className="flex flex-col items-center justify-center gap-8 m-auto min-h-screen">
+      <h1 className="text-5xl font-bold">
+        Welcome to Appnovia Starter Template
+      </h1>
+      <p className="text-2xl">Edit this page and begin creating</p>
+    </section>
+  );
 }
