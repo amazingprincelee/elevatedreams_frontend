8c7b34d5a27980e6ab17a3b343d459a9045078d6
Chioma Ezeamama <58475309+Chiomsyn@users.noreply.github.com>
2024-08-31 18:26:04 +0100

    chore: installed biome formatter

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import React from "react";
+import type React from "react";
 import { NextUIProvider } from "@nextui-org/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+	return <NextUIProvider>{children}</NextUIProvider>;
 }
