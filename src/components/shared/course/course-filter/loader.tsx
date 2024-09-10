489ff294158fffa0f7c38c0d164bc631e2b90d2f
Chioma Ezeamama <58475309+Chiomsyn@users.noreply.github.com>
2024-09-10 17:07:16 +0100

    fix

--- src/components/shared/course/course-filter/loader.tsx
+++ src/components/shared/course/course-filter/loader.tsx
@@ -6,12 +6,12 @@ type Props = {}
 const CardLoader: FC<Props> = ({}) => {
   return (
     <div className="shadow-sm flex flex-col  h-[580px] w-fit rounded-xl gap-3 pb-5">
-      <Skeleton className="h-full w-full rounded-t-xl" />
+      <Skeleton className="h-full rounded-t-xl w-[360px]" />
       <div className="grid gap-4 px-4">
         <Skeleton className="h-4 w-[250px]" />
         <Skeleton className="h-2 w-28" />
         <Skeleton className="h-[100px] w-full" />
-        <div className="flex-center gap-1 mt-4]1 justify-between">
+        <div className="flex-center gap-1 mt-4 justify-between">
           <Skeleton className="h-10 rounded-xl w-28" />
           <Skeleton className="h-10 rounded-xl w-28" />
           <Skeleton className="h-10 rounded-xl w-28" />
