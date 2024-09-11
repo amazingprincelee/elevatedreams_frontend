dcd314bc4018b1ca6cbd7dfb6d03168d3c236533
Amazing Prince Lee <prince_lee.nna85@ymail.com>
2024-09-11 10:13:19 +0100

    new update

--- src/components/shared/learning-journey/learning-journey.tsx
+++ src/components/shared/learning-journey/learning-journey.tsx
@@ -1,46 +1,12 @@
 import React from 'react'
 import { BsStars } from 'react-icons/bs'
 import LinkButton from '../button/link-button'
+import SvgEffect from './svg-background-effect'
 
 const LearningJourney = () => {
   return (
     <div className="container mb-8 relative bg-[#1a0b61] text-white min-h-[500px] rounded-3xl overflow-hidden flex flex-col items-center justify-center">
-      <svg
-        className="absolute inset-0 w-full h-full"
-        xmlns="http://www.w3.org/2000/svg"
-        preserveAspectRatio="none"
-      >
-        <path
-          fill="none"
-          stroke="rgba(255,255,255,0.1)"
-          stroke-width="2"
-          d="M0 80 C 300 120, 600 40, 900 80 S 1440 160 1800 80"
-        />
-        <path
-          fill="none"
-          stroke="rgba(255,255,255,0.1)"
-          stroke-width="2"
-          d="M0 160 C 300 200, 600 120, 900 160 S 1440 240 1800 160"
-        />
-        <path
-          fill="none"
-          stroke="rgba(255,255,255,0.1)"
-          stroke-width="2"
-          d="M0 240 C 300 280, 600 200, 900 240 S 1440 320 1800 240"
-        />
-        <path
-          fill="none"
-          stroke="rgba(255,255,255,0.1)"
-          stroke-width="2"
-          d="M0 320 C 300 360, 600 280, 900 320 S 1440 400 1800 320"
-        />
-        <path
-          fill="none"
-          stroke="rgba(255,255,255,0.1)"
-          stroke-width="2"
-          d="M0 400 C 300 440, 600 360, 900 400 S 1440 480 1800 400"
-        />
-      </svg>
+      <SvgEffect />
 
       <div className="relative z-10 flex flex-col items-center justify-center font-jakarta  h-full">
         <div className="flex flex-row p-2">
