5e490da87eae53f4b9ae88001845a23869dc230a
Amazing Prince Lee <prince_lee.nna85@ymail.com>
2024-09-11 14:52:18 +0100

    applied responsiveness

--- src/components/shared/footer/footer.tsx
+++ src/components/shared/footer/footer.tsx
@@ -10,64 +10,64 @@ import {
   FaYoutube,
 } from 'react-icons/fa6'
 
-const footer = () => {
+const Footer = () => {
   return (
-    <div>
-      <div className="container bg-violet-300 rounded-[40px] p-24 mb-8">
-        <div className="flex gap-24">
-          <div className="w-[500px]">
-            <Image src="/images/logo.png" width={116} height={52} alt="logo" />
+    <div className="bg-violet-300 rounded-[40px] p-8 sm:p-12 lg:p-24 mb-8">
+      <div className="flex flex-col sm:flex-row lg:flex-row gap-12 lg:gap-24">
+        <div className="lg:w-[450px]">
+          <Image src="/images/logo.png" width={116} height={52} alt="logo" />
 
-            <p className="w-96 mt-8">
-              No 4a Etim Okpoyo close, off Aba Express road by total filling
-              station, Port harcourt, rivers State
-            </p>
+          <p className="mt-8 max-w-xs lg:max-w-none w-96">
+            No 4a Etim Okpoyo close, off Aba Express road by total filling
+            station, Port Harcourt, Rivers State
+          </p>
+        </div>
+        <div className="lg:w-96 flex flex-col ">
+          <h2 className="font-bold ml-0 mb-6 text-xl">Pages</h2>
+          <ul>
+            <li className="mb-4">About Us</li>
+            <li className="mb-4">Courses</li>
+            <li className="mb-4">Our Team</li>
+            <li className="mb-4">Blogs</li>
+            <li className="mb-4">Contact Us</li>
+          </ul>
+        </div>
+        <div className="p-2 lg:w-96">
+          <h2 className="font-bold mb-6 text-xl">Contact Us</h2>
+          <div className="flex gap-2 mb-6">
+            <div className="bg-pink-300 p-2 rounded-full">
+              <FaRegEnvelope color="red" className="text-2xl" />
+            </div>
+            <p>customercare@loctechng.com</p>
           </div>
-          <div className="w-96">
-            <h2 className="font-bold mb-6 text-xl">Pages</h2>
-            <ul>
-              <li className="mb-6">About Us</li>
-              <li className="mb-6">Courses</li>
-              <li className="mb-6">Our Team</li>
-              <li className="mb-6">Blogs</li>
-              <li className="mb-6">Contact Us</li>
-            </ul>
+          <div className="flex gap-2 mb-6">
+            <div className="bg-pink-300 p-2 rounded-full">
+              <FaPhone color="red" className="text-2xl" />
+            </div>
+            <p>+234 123 456 7890</p>
           </div>
-          <div className="p-2 w-96">
-            <h2 className="font-bold mb-8 text-xl">Contact Us</h2>
-            <div className="flex gap-2 mb-8">
-              <p className="bg-pink-300 p-2 rounded-full">
-                <FaRegEnvelope color="red" className="text-2xl " />
-              </p>
-              <p>customercare@loctechng.com</p>
+          <div className="flex gap-2">
+            <div className="bg-pink-300 p-2 rounded-full">
+              <FaClock color="red" className="text-2xl" />
             </div>
-            <div className="flex gap-2 mb-8">
-              <p className="bg-pink-300 p-2 rounded-full">
-                <FaPhone color="red" className="text-2xl" />
-              </p>
-              <p>customercare@loctechng.com</p>
-            </div>
-            <div className="flex gap-2">
-              <p className="bg-pink-300 p-2 rounded-full">
-                <FaClock color="red" className="text-2xl" />
-              </p>
-              <p>customercare@loctechng.com</p>
-            </div>
+            <p>Mon - Fri: 9:00 AM - 6:00 PM</p>
           </div>
         </div>
+      </div>
 
-        <div className="flex flex-col justify-center items-center mt-40">
-          <div className="flex gap-6 mb-8">
-            <FaXTwitter className="text-lg" />
-            <FaInstagram className="text-lg" />
-            <FaLinkedin className="text-lg" />
-            <FaYoutube className="text-lg" />
-          </div>
-          <p>© 2024 Crafelo Studios. All rights reserved.</p>
+      <div className="flex flex-col items-center mt-16">
+        <div className="flex gap-6 mb-8">
+          <FaXTwitter className="text-lg" />
+          <FaInstagram className="text-lg" />
+          <FaLinkedin className="text-lg" />
+          <FaYoutube className="text-lg" />
         </div>
+        <p className="text-center">
+          © 2024 Crafelo Studios. All rights reserved.
+        </p>
       </div>
     </div>
   )
 }
 
-export default footer
+export default Footer
