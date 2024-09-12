cdee1145fade441212b15bb34ace3d1c39a88118
Chioma Ezeamama <58475309+Chiomsyn@users.noreply.github.com>
2024-09-12 11:04:15 +0100

    Implementing blog card

--- src/components/shared/blog/blog.tsx
+++ src/components/shared/blog/blog.tsx
@@ -8,17 +8,29 @@ import BlogLoader from './loader'
 type Props = { row?: number; showPagination?: boolean }
 const Blog: FC<Props> = ({ row = 1, showPagination }) => {
   const [blogs, setBlogs] = useState(['', '', '', ''])
+
   const {
     windowSize: { width },
   } = useWindowSize()
 
-  const count = width !== undefined && width > 1024 && width < 1660 ? 3 : 4
+  const count =
+    typeof width === 'number'
+      ? width > 1024 && width < 1660
+        ? 3
+        : width > 300 && width < 1024
+          ? 2
+          : 4
+      : 0
+
   const totalRow = row * count
 
   const blogList = blogs.slice(0, totalRow)
+
   return (
-    <div className="def-contain flex flex-col gap-5 md:gap-10 mt-10 mb-20">
-      <h1 className="font-bold text-4xl text-center">Latest from our Blogs</h1>
+    <div className="def-contain flex flex-col gap-5 md:gap-10 mt-10 mb-20 ">
+      <h1 className="md:font-black text-3xl md:text-4xl text-center">
+        Latest from our Blogs
+      </h1>
       <div className="responsive-grid">
         {blogList.length < 1
           ? Array.from({ length: totalRow }, (_, i) => <BlogLoader key={i} />)
