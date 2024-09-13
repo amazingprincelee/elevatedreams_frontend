dd5b17af2c674c5e1ea04a4fbff2cd192a260763
Chioma Ezeamama <58475309+Chiomsyn@users.noreply.github.com>
2024-09-13 11:50:37 +0100

    fixed learning journey component

--- src/components/shared/learning-journey/learning-journey.jsx
+++ src/components/shared/learning-journey/learning-journey.jsx
@@ -1,42 +1,42 @@
-import React from 'react'
-import { BsStars } from 'react-icons/bs'
-import LinkButton from '../button/link-button'
-import SvgEffect from './svg-background-effect'
-
-
+import React from "react";
+import { BsStars } from "react-icons/bs";
+import LinkButton from "../button/link-button";
+import SvgEffect from "./svg-background-effect";
 
 const LearningJourney = () => {
   return (
-    <div className="container mb-8 relative bg-[#1a0b61] text-white min-h-[500px] rounded-3xl overflow-hidden flex flex-col items-center justify-center">
-      <SvgEffect />
+    <div className="def-contain mt-10 lg:mt-20 lg:mb-20 mb-10">
+      <div className="container mb-8 relative bg-[#1a0b61] text-white min-h-[500px] rounded-3xl overflow-hidden flex flex-col items-center justify-center">
+        <SvgEffect />
 
-      <div className="relative z-10 flex flex-col items-center justify-center font-jakarta  h-full">
-        <div className="flex flex-row p-2">
-          <BsStars size={24} />
-          <p className="mb-4 text-sm ml-2 ">
-            {' '}
-            Pioneer in the field of IT Training
+        <div className="relative z-10 flex flex-col items-center justify-center font-jakarta  h-full">
+          <div className="flex flex-row p-2">
+            <BsStars size={24} />
+            <p className="mb-4 text-sm ml-2 font-light">
+              {" "}
+              Pioneer in the field of IT Training
+            </p>
+          </div>
+
+          <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold mb-4 mt-4 text-center">
+            Start your learning journey for Free!
+          </h1>
+
+          <p className="md:text-lg text-base md:font-light font-[10] mb-12 mt-3 text-center">
+            Get certified in our industry standard course and smash all your
+            tech goals.
           </p>
+
+          <LinkButton
+            url={"#"}
+            label={"Get Started"}
+            isIcon={true}
+            className="w-[#177px]"
+          />
         </div>
-
-        <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold mb-4 mt-4 text-center">
-          Start your learning journey for Free!
-        </h1>
-
-        <p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl mb-12 mt-3 text-center">
-          Get certified in our industry standard course and smash all your tech
-          goals.
-        </p>
-
-        <LinkButton
-          url={'#'}
-          label={'Get Started'}
-          isIcon={true}
-          className="w-[#177px]"
-        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LearningJourney
+export default LearningJourney;
