cabf985ed871d7891a9c9ad197a0bd4ad04bdb31
Chioma Ezeamama <58475309+Chiomsyn@users.noreply.github.com>
2024-09-13 10:38:21 +0100

    working on FAQ section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 'use client'
 
 import Blog from '@/components/shared/blog/blog'
-import BlogCard from '@/components/shared/blog/blog-card'
-import LinkButton from '@/components/shared/button/link-button'
 import CourseFilter from '@/components/shared/course/course-filter/course-filter'
+import Faq from '@/components/shared/faq/faq'
 import HomeHero from '@/components/shared/hero/home/home'
 import LearningJourney from '@/components/shared/learning-journey/learning-journey'
 import OfferingCourseCard from '@/components/shared/offering-course-card/offering-course-card'
@@ -13,14 +12,12 @@ export default function Home() {
   return (
     <div className="bg-white">
       <HomeHero />
-
-      <section className="flex flex-col items-center justify-center gap-8 m-auto min-h-screen">
-        <OfferingCourseCard />
-        <CourseFilter />
-        <ReliedUpon />
-        <Blog />
-        <LearningJourney />
-      </section>
+      <OfferingCourseCard />
+      <CourseFilter />
+      <ReliedUpon />
+      <Blog />
+      <Faq />
+      <LearningJourney />
     </div>
   )
 }
