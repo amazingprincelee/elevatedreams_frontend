b81eac597f1d085e521c0b85295ab8f625e29d68
Chioma Ezeamama <58475309+Chiomsyn@users.noreply.github.com>
2024-09-15 02:59:23 +0100

    implemented custom scroll bar for course list

--- src/components/shared/course/course-filter/course-filter.tsx
+++ src/components/shared/course/course-filter/course-filter.tsx
@@ -10,7 +10,7 @@ type Props = { courses: CourseProps[] }
 const CourseFilter: FC<Props> = ({ courses }) => {
   const [selected, setSelected] = useState<string>('featured')
 
-  const filters = [
+  const defaultFilters = [
     'Featured',
     'Software Development',
     'Data Science',
@@ -21,6 +21,23 @@ const CourseFilter: FC<Props> = ({ courses }) => {
     'Animation',
   ]
 
+  const uniqueCategories = courses
+    .map((e) => e.category)
+    .filter((value, index, self) => self.indexOf(value) === index)
+
+  const filters =
+    courses.length > 0 ? ['Featured', ...uniqueCategories] : defaultFilters
+
+  const selectedCourses = courses.filter((el) => {
+    return el.category.toLowerCase() === selected
+      ? el
+      : selected === 'featured' && el.featured === true
+        ? el
+        : null
+  })
+
+  console.log(filters)
+
   return (
     <div className="w-full bg-base pt-10 pb-20">
       <motion.div
@@ -29,13 +46,14 @@ const CourseFilter: FC<Props> = ({ courses }) => {
         animate={{ opacity: [0, 1], translateY: [100, 0] }}
         transition={{ delay: 0.4, duration: 1, ease: 'easeInOut' }}
         viewport={{ once: true }}
+        className="def-contain"
       >
         <Tabs
           onValueChange={(value: string) => setSelected(value)}
           defaultValue="featured"
-          className="w-full grid gap-5"
+          className="w-full grid grid-cols-1 gap-5 "
         >
-          <TabsList className="w-full gap-5 overflow-scroll def-contain">
+          <TabsList className=" overflow-x-scroll gap-5  ">
             {filters.map((el: string, index: number) => (
               <TabsTrigger
                 key={index}
@@ -46,9 +64,11 @@ const CourseFilter: FC<Props> = ({ courses }) => {
               </TabsTrigger>
             ))}
           </TabsList>
-          <TabsContent value="featured">
-            <CoursesList courses={courses} row={2} />
-          </TabsContent>
+          {filters.map((el, index) => (
+            <TabsContent key={index} value={el.toLowerCase()}>
+              <CoursesList courses={selectedCourses} row={2} />
+            </TabsContent>
+          ))}
         </Tabs>
       </motion.div>
     </div>
