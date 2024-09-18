d2cb3767f6574cba89a3981dfcffae21913c6c6d
Amazing Prince Lee <prince_lee.nna85@ymail.com>
2024-09-18 20:14:43 +0100

    update login and signup component

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import Auth from '@/components/shared/auth-component/auth'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Auth />
+    </div>
+  )
+}
+
+export default page
