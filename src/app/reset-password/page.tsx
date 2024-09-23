bd67a84471054fcda2dd0d17028b874203163855
Amazing Prince Lee <prince_lee.nna85@ymail.com>
2024-09-23 12:16:03 +0100

    chores(auth): import component in the various pages

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,85 +1,12 @@
-import { Button } from '@/components/shared/button/button'
-import InputField from '@/components/ui/form-fields/input-field'
-import { yupResolver } from '@hookform/resolvers/yup'
-import Image from 'next/image'
-import Link from 'next/link'
+import ResetPassword from '@/components/shared/auth-component/reset-password'
 import React from 'react'
-import { Controller, useForm } from 'react-hook-form'
-import { MdEmail } from 'react-icons/md'
-import * as yup from 'yup'
 
-export const schema = yup.object({
-  email: yup.string().required('Email is required').email('Invalid email'),
-})
-
-const ResetPassword = () => {
-  const {
-    handleSubmit,
-    formState: { errors },
-    control,
-  } = useForm({
-    defaultValues: { email: '' },
-    resolver: yupResolver(schema),
-  })
-
-  const handleReset = async (data: { email: string }) => {
-    // Handle reset logic here
-  }
-
+const page = () => {
   return (
-    <div className="def-contain flex justify-center items-center">
-      <div className="bg-white shadow-lg rounded-lg overflow-hidden w-full grid grid-cols-1 md:grid-cols-[40%_60%]">
-        {/* Left side - Reset Password form */}
-        <div className="w-full md:px-9 lg:px-16">
-          <h2 className="text-2xl font-semibold mb-4">Reset Password</h2>
-          <p className="text-sm text-gray-600 mb-6">
-            We will send a <strong>4 digit code</strong> to your email address.
-          </p>
-
-          <form onSubmit={handleSubmit(handleReset)} className="space-y-6">
-            <Controller
-              name="email"
-              control={control}
-              render={({ field }) => (
-                <InputField
-                  name="email"
-                  placeholder="Email"
-                  value={field.value}
-                  errors={errors}
-                  field={field}
-                  className="w-full"
-                  startContent={
-                    <MdEmail className="text-xl text-gray-400 pointer-events-none" />
-                  }
-                />
-              )}
-            />
-
-            <Button className="w-full bg-red-600 text-white py-2" size={'lg'}>
-              Send reset email
-            </Button>
-          </form>
-
-          <div className="text-center mt-4">
-            <Link href="/login" className="text-sm text-red-600">
-              Remember password?
-            </Link>
-          </div>
-        </div>
-
-        {/* Right side - Image */}
-        <div className="hidden md:block">
-          <Image
-            src="/images/auth/young-people.png"
-            alt="Person with laptop"
-            width={500}
-            height={500}
-            className="object-cover w-full h-full"
-          />
-        </div>
-      </div>
+    <div>
+      <ResetPassword />
     </div>
   )
 }
 
-export default ResetPassword
+export default page
--- src/app/verify-otp/pages.tsx
+++ src/app/verify-otp/pages.tsx
@@ -1,111 +1,12 @@
-import { Button } from '@/components/shared/button/button'
-import { yupResolver } from '@hookform/resolvers/yup'
-import Image from 'next/image'
+import VerifyOtp from '@/components/shared/auth-component/otp-verification'
 import React from 'react'
-import { Controller, useForm } from 'react-hook-form'
-import * as yup from 'yup'
 
-export const schema = yup.object({
-  otp0: yup
-    .string()
-    .required('OTP digit is required')
-    .length(1, 'Must be 1 digit'),
-  otp1: yup
-    .string()
-    .required('OTP digit is required')
-    .length(1, 'Must be 1 digit'),
-  otp2: yup
-    .string()
-    .required('OTP digit is required')
-    .length(1, 'Must be 1 digit'),
-  otp3: yup
-    .string()
-    .required('OTP digit is required')
-    .length(1, 'Must be 1 digit'),
-})
-
-const VerifyOtp = () => {
-  const {
-    handleSubmit,
-    formState: { errors },
-    control,
-  } = useForm({
-    defaultValues: { otp0: '', otp1: '', otp2: '', otp3: '' },
-    resolver: yupResolver(schema),
-  })
-
-  const handleOtpSubmit = async (data: {
-    otp0: string
-    otp1: string
-    otp2: string
-    otp3: string
-  }) => {
-    const otp = `${data.otp0}${data.otp1}${data.otp2}${data.otp3}` // Combine the 4 digits into a single OTP
-    console.log('Submitted OTP:', otp)
-    // Handle OTP verification logic here
-  }
-
+const pages = () => {
   return (
-    <div className="def-contain flex justify-center items-center">
-      <div className="bg-white shadow-lg rounded-lg overflow-hidden w-full grid grid-cols-1 md:grid-cols-[40%_60%]">
-        {/* Left side - OTP Verification */}
-        <div className="w-full md:px-9 lg:px-16">
-          <h2 className="text-2xl font-semibold mb-6">Verify OTP</h2>
-          <p className="text-sm text-gray-600 mb-4">
-            Confirm the OTP sent to you
-          </p>
-
-          <form onSubmit={handleSubmit(handleOtpSubmit)} className="space-y-6">
-            <div className="flex space-x-4 justify-center">
-              {Array.from({ length: 4 }).map((_, idx) => (
-                <Controller
-                  key={idx}
-                  name="otp0"
-                  control={control}
-                  render={({ field }) => (
-                    <input
-                      {...field}
-                      type="text"
-                      maxLength={1}
-                      className={`border p-4 text-xl font-semibold text-center w-14 h-14 rounded-lg ${
-                        errors.otp0 ? 'border-red-500' : 'border-gray-300'
-                      }`}
-                      placeholder="-"
-                    />
-                  )}
-                />
-              ))}
-            </div>
-
-            {errors.otp0 && (
-              <p className="text-red-500 text-sm text-center">
-                {errors.otp0.message}
-              </p>
-            )}
-
-            <Button className="w-full bg-red-600 text-white py-2" size={'lg'}>
-              Verify OTP
-            </Button>
-          </form>
-
-          <div className="text-center mt-4">
-            <button className="text-sm text-red-600">Resend OTP</button>
-          </div>
-        </div>
-
-        {/* Right side - Image */}
-        <div className="hidden md:block">
-          <Image
-            src="/images/auth/young-people2.png"
-            alt="Person with laptop"
-            width={500}
-            height={500}
-            className="object-cover w-full h-full"
-          />
-        </div>
-      </div>
+    <div>
+      <VerifyOtp />
     </div>
   )
 }
 
-export default VerifyOtp
+export default pages
