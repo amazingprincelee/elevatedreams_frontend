eff43652bae64ffe503a0f60b2b63bca63281b32
Chioma Ezeamama <58475309+Chiomsyn@users.noreply.github.com>
2024-09-24 10:01:24 +0100

    fix: worked on blog hero

--- src/components/pages/blogs/hero.tsx
+++ src/components/pages/blogs/hero.tsx
@@ -1,38 +1,39 @@
-import { buttonVariants } from '@/components/shared/button/button'
-import { ArrowLeft, Clock } from 'lucide-react'
-import Image from 'next/image'
 import { FC } from 'react'
 
 type Props = {}
 const HeroComp: FC<Props> = ({}) => {
   return (
-    <div className="w-full h-[665px] flex lg:h-[400px] pt-32 bg-base my-auto">
+    <div className="w-full h-[665px] flex lg:h-[300px] pt-32 my-auto">
       <div className="def-contain w-full">
-        <p className="self-start flex gap-2 items-center">
-          <ArrowLeft className="size-5" />
-          Go back to <span className="text-primary">Blog</span>
+        <p className="self-start">
+          Go back to <span className="text-primary">loctechng.com</span>
         </p>
         <div className="flex flex-col space-y-4 items-center">
-          <div className={buttonVariants()}>Web Debelopment</div>
-          <h1 className="font-sans text-5xl font-bold w-[50%] text-center">
-            The importance of good leadership in a growing business
-          </h1>
-          <div className="flex-center gap-4 mt-2">
-            <div className="flex-center space-x-2">
-              <Clock className="size-4" />
-              <p className="text-xs text-gray-400">Mar 15, 2022</p>
+          <h1 className="font-sans text-4xl font-bold">Blog</h1>
+          {/* Search Bar */}
+          <div className="relative">
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+              {/* Search Icon */}
+              <svg
+                className="h-5 w-5 text-blue-500"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1"
+                  d="M11 4a7 7 0 100 14 7 7 0 000-14zM21 21l-4.35-4.35"
+                />
+              </svg>
             </div>
-
-            <div className="flex-center space-x-2">
-              <Image
-                src={'/images/oil.jpg'}
-                alt={'blog'}
-                width={1200}
-                height={900}
-                className="size-5 object-cover rounded-full"
-              />
-              <h3 className="font-medium text-xs">Omotola Iwawumi</h3>
-            </div>
+            <input
+              className="w-full pl-10 pr-4 py-2 min-w-80 rounded-full border-[0.5px] sm:text-xs placeholder:italic placeholder:text-slate-400 border-gray-300 focus:outline-none focus:border-primary/50"
+              type="text"
+              placeholder="Search anything..."
+            />
           </div>
         </div>
       </div>
