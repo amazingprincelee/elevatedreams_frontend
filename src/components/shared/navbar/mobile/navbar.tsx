f1d1abcfe7371ba336669bfe95f8858a01d82de4
Chioma Ezeamama <58475309+Chiomsyn@users.noreply.github.com>
2024-09-25 13:22:58 +0100

    worked on dashboard

--- src/components/shared/navbar/mobile/navbar.tsx
+++ src/components/shared/navbar/mobile/navbar.tsx
@@ -2,7 +2,7 @@
 
 import { motion } from 'framer-motion'
 import Link from 'next/link'
-import { FC } from 'react'
+import { FC, useRef } from 'react'
 import { linksData } from '..'
 import LinkButton from '../../button/link-button'
 
@@ -12,73 +12,87 @@ type Props = {
   path: string
 }
 const MobileNavbar: FC<Props> = ({ isOpen, setIsOpen, path }) => {
+  const contentRef = useRef<HTMLDivElement>(null)
   return (
     <div
-      className={`bg-gray-500/70 z-30 fixed transition duration-[2000ms] delay-150 ease-in-out h-full ${
-        isOpen === false
-          ? ' opacity-0 -translate-x-0'
-          : ' translate-x-100 opacity-100 '
-      } w-full  `}
+      className={` z-30 grid grid-cols-[70%_30%] md:grid-cols-[50%_50%] fixed transition duration-[2000ms] delay-150 ease-in-out h-full ${
+        isOpen
+          ? 'translate-x-[0] lg:-translate-x-[100%]'
+          : '-translate-x-[100%]'
+      } w-full  block lg:hidden`}
     >
-      <div className={`bg-white  h-full w-[75%] px-6  md:w-1/2 pt-5`}>
-        <div className="">
-          {linksData.map((item, index) => {
-            return (
-              <Link
-                href={item.url}
-                onClick={() => setIsOpen(false)}
+      <motion.div
+        initial={false}
+        transition={{
+          ease: 'easeInOut',
+          duration: 0.4,
+          delay: 0.5,
+        }}
+        animate={{
+          opacity: [0, 1],
+          translateX: [-100, 0],
+        }}
+        // style={{ overflow: "hidden" }}
+        className={`bg-white  h-full px-6 pt-5`}
+      >
+        {linksData.map((item, index) => {
+          return (
+            <Link href={item.url} onClick={() => setIsOpen(false)} key={index}>
+              <motion.div
+                transition={{
+                  ease: 'easeInOut',
+                  duration: 0.4,
+                  delay: 0.2 + 0.2 * index,
+                }}
+                animate={{
+                  opacity: [0, 1],
+                  translateX: [-100, 0],
+                }}
                 key={index}
+                className="flex-gap gap-3 py-4"
               >
-                <motion.div
-                  transition={{
-                    ease: 'easeInOut',
-                    duration: 0.4,
-                    delay: 0.2 + 0.2 * index,
-                  }}
-                  animate={{
-                    opacity: [0, 1],
-                    translateX: [-100, 0],
-                  }}
-                  key={index}
-                  className="flex-gap gap-3 py-4"
+                <p
+                  className={`${
+                    path !== item.url ? 'text-gray-600' : 'text-primary'
+                  }`}
                 >
-                  <p
-                    className={`${
-                      path !== item.url ? 'text-gray-600' : 'text-primary'
-                    }`}
-                  >
-                    {item.name}
-                  </p>
-                </motion.div>
-              </Link>
-            )
-          })}
-          <motion.div
-            transition={{
-              ease: 'easeInOut',
-              duration: 0.7,
-              delay: 1.4,
-            }}
-            animate={{
-              opacity: [0, 1],
-              translateX: [-100, 0],
-            }}
-          >
-            <div className="grid gap-2 mt-10">
-              <LinkButton
-                url={'#'}
-                label={'Log in'}
-                className="border w-full border-primary bg-transparent text-primary hover:text-white py-3"
-              ></LinkButton>
-              <LinkButton
-                url={'#'}
-                label={'Get Started'}
-                className=" w-full border py-3"
-              ></LinkButton>
-            </div>
-          </motion.div>
-        </div>
-      </div>
+                  {item.name}
+                </p>
+              </motion.div>
+            </Link>
+          )
+        })}
+        <motion.div
+          transition={{
+            ease: 'easeInOut',
+            duration: 0.7,
+            delay: 1.4,
+          }}
+          animate={{
+            opacity: [0, 1],
+            translateX: [-100, 0],
+          }}
+        >
+          <div className="grid gap-2 mt-10">
+            <LinkButton
+              url={'/login'}
+              label={'Log in'}
+              className="border w-full border-primary bg-transparent text-primary hover:text-white py-3"
+            ></LinkButton>
+            <LinkButton
+              url={'/register'}
+              label={'Get Started'}
+              className=" w-full border py-3"
+            ></LinkButton>
+          </div>
+        </motion.div>
+      </motion.div>
+      {isOpen && (
+        <div
+          className="bg-gray-500/70 h-full"
+          onClick={() => setIsOpen(false)}
+        ></div>
+      )}
     </div>
   )
 }
