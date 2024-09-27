4fb5c67bb654e32a2a136fa95d89150796aec036
amazingprincelee <prince_lee.nna85@ymail.com>
2024-09-27 10:23:44 -0700

    feature: Dashboard navbar and Dasboard footer

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,7 +1,5 @@
 import ContactForm from '@/components/pages/contact-us/contact-form'
 import HeroContactUs from '@/components/pages/contact-us/hero'
-import DashboardFooter from '@/components/shared/footer/dashboard-footer'
-import DashboardNavbar from '@/components/shared/navbar/dashboard-navbar'
 import { FC } from 'react'
 
 type Props = {}
