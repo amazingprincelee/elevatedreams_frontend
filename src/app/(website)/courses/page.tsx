7fb8aa6db36e0363fddcd11adcb2a0b899929ec0
Prince Lee <prince_lee.nna85@ymail.com>
2024-12-23 11:42:46 +0100

    commented backend services

--- src/app/(website)/courses/page.tsx
+++ src/app/(website)/courses/page.tsx
@@ -5,24 +5,24 @@ import CourseFilter from '@/components/shared/course/course-filter/course-filter
 import LearningJourney from '@/components/shared/learning-journey/learning-journey'
 import TopCourses from '@/components/shared/top-courses/top-courses'
 import { FC, Suspense, useEffect, useState } from 'react'
-import { get } from '../../../../backend_services/api_services'
+// import { get } from '../../../../backend_services/api_services'
 
 type Props = {}
 const CoursesPage: FC<Props> = ({}) => {
   const [courses, setCourses] = useState<any[]>([])
 
-  useEffect(() => {
-    async function fetchApiData() {
-      try {
-        const apiData: any = await get('/course')
-        if (apiData && apiData.data) setCourses(apiData.data)
-      } catch (error) {
-        //    toast.error("Please check your internet connectivity");
-      }
-    }
+  // useEffect(() => {
+  //   async function fetchApiData() {
+  //     try {
+  //       const apiData: any = await get('/course')
+  //       if (apiData && apiData.data) setCourses(apiData.data)
+  //     } catch (error) {
+  //       //    toast.error("Please check your internet connectivity");
+  //     }
+  //   }
 
-    fetchApiData()
-  }, [])
+  //   fetchApiData()
+  // }, [])
   return (
     <Suspense>
       <CoursesHero />
